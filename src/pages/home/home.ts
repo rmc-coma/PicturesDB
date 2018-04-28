bb9f171023d69d723f6f08579806ea7898343c76
rmc-coma <rmc-coma@student.42.fr>
2018-04-28 17:45:12 +0200

    Added proxy

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 import { NavController } from 'ionic-angular';
-import { map } from 'rxjs/operators';
-
-
 
 @Component({
   selector: 'page-home',
@@ -14,20 +11,16 @@ export class HomePage {
   public rep: any;
 
   constructor(public navCtrl: NavController, private http: Http) {
-    this.http.get('http://46.101.55.225:6686/routes').pipe(
-      map(res => res.json())
-    ).subscribe(
-      response => {
-        //this.response = response;
-        //console.log(response);
-        this.rep = response;
-      }
-    );
+
   }
 
-  kek()
+  ionViewDidEnter()
   {
-
-    console.log(this.rep);
+    this.http.get("api/root").subscribe(
+      response => {
+        this.rep = (response);
+      }
+    );
+    return this.rep;
   }
 }
